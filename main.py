773ca429c71551bdda961de38b0cd9496573cae6
Neeraj <neeraj.cseh@gmail.com>
2026-02-08 22:51:40 +0530

    add

---
+++ main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def root():
+    return {"message" : "Welcome to my API"}
+
+@app.get("/")
+def get_posts():
+    return {"data" : "Your posts are here."}
