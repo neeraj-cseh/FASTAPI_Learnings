780ed2614e0e52005d86ad80167e67438d133e64
Neeraj <neeraj.cseh@gmail.com>
2026-02-08 23:15:25 +0530

    add

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ app = FastAPI()
 def root():
     return {"message" : "Welcome to my API"}
 
-@app.get("/")
+@app.get("/posts")
 def get_posts():
     return {"data" : "Your posts are here."}
+
+@app.post("/createposts")
+def create_posts():
+    return {"message" : "successful"}
